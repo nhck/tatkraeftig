be75220d768b436d6115453f2df2bce9c492919e
nhck <n@nhck.de>
2013-12-08 08:45:32 +0100

    One stepforward two steps back

--- src/tatk.php
+++ src/tatk.php
@@ -1,32 +1,31 @@
 <?php
+
 /**
  * @package Tatkraeftig
  * @version 1
  */
 /*
-Plugin Name: Tatkraeftig Project loader
-Depends:WordPress-to-Lead for Salesforce CRM
-Plugin URI: http://tatkraeftig.org/
-Description: Plugin loads http://tatkraeftig.org/ projects
-Author: RHOKHH
-Version: 1
-Author URI: http://rhokhh.org
-*/
+  Plugin Name: Tatkraeftig Project loader
+  Depends:WordPress-to-Lead for Salesforce CRM
+  Plugin URI: http://tatkraeftig.org/
+  Description: Plugin loads http://tatkraeftig.org/ projects
+  Author: RHOKHH
+  Version: 1
+  Author URI: http://rhokhh.org
+ */
 
 /**
-* File Structure:
-* 1) Functionality
-* 2) Handlers
-*/
-
-
-
+ * File Structure:
+ * 1) Functionality
+ * 2) Handlers
+ */
 require_once( ABSPATH . 'wp-content/plugins/salesforce-wordpress-to-lead/salesforce.php' );
 
 /*
  * Class 
  * Provides Wrapper, so functions can have human names
  */
+
 class tatkraeftig {
 
   var $oauthToken;
@@ -34,25 +33,24 @@ class tatkraeftig {
   function __construct() {
 
     add_shortcode('projekte', array($this, 'display'));
-
   }
 
   function login() {
-    
+
     require_once("settings.php");
 
     $url = 'https://login.salesforce.com/services/oauth2/token';
     $args = array(
-        'method' => 'POST',
-        'body' => array(
-            'grant_type' => 'password',
-            'username' => $username,
-            'password' => $password,
-            'client_id' => $client_id,
-            'client_secret' => $client_secret
+      'method' => 'POST',
+      'body' => array(
+        'grant_type' => 'password',
+        'username' => $username,
+        'password' => $password,
+        'client_id' => $client_id,
+        'client_secret' => $client_secret
         ));
 
-    $request = wp_remote_post( $url, $args );
+    $request = wp_remote_post($url, $args);
     $requestJson = json_decode($request['body'], true);
     $this->oauthToken = $requestJson['access_token'];
   }
@@ -64,62 +62,97 @@ class tatkraeftig {
    * @param none
    * @return array
    */
-	function getCampaigns() {
 
-    if(empty($this->oauthToken)) {
+  function getCampaigns() {
+
+    if (empty($this->oauthToken)) {
       $this->login();
     }
 
     $urlParams = urlencode('SELECT Name, Id, Description from Campaign');
     $url = 'https://eu2.salesforce.com/services/data/v29.0/query?q=' . $urlParams;
     $args = array(
-          'method' => 'GET',
-          'headers' => array( 'Authorization:' => 'Bearer ' . $this->oauthToken )
-        );
-    
-    $request = wp_remote_get( $url, $args );
+      'method' => 'GET',
+      'headers' => array('Authorization:' => 'Bearer ' . $this->oauthToken)
+    );
+
+    $request = wp_remote_get($url, $args);
     $requestJson = json_decode($request['body'], true);
-    
+
     return $requestJson;
   }
-	
+
   /*
    * Prints Array retrieved via {@see getdata}
    * 
    * @param array Array that was retrieved
    * @return string
    */
+
   function printdata($data = array()) {
-    
+
     return 'hello';
   }
 
   function createForm($campaignID) {
     $options = get_option('salesforce2');
-    $options['forms'][1]['inputs']['Campaign_ID']['value'] = $campaignID; 
-    return salesforce_form($options) . '<pre>' . print_r($options, true) . '</pre>';
+    $options['forms'][1]['inputs']['Campaign_ID']['value'] = $campaignID;
+    return salesforce_form($options) . '<pre><h4>$options</h4>' . print_r($options, true) . '</pre>' . '<pre><h4>Submitted Options</h4>' . print_r($_POST, true) . '</pre>';
   }
 
-  function display(){
+  function submission() {
+    $options = get_option("salesforce2");
+
+    //validation
+    foreach ($options['forms'][1]['inputs'] as $key => $input) {
+
+      $val = trim($_POST[$key]);
+      if ($input['required'] && !$val) {
+        $options['forms'][$form]['inputs'][$key]['error'] = true;
+        $error = true;
+      }
+      else if ($key == 'email' && $input['required'] && !is_email($_POST[$key])) {
+        $error = true;
+        $emailerror = true;
+      }
+      else {
+        if (isset($_POST[$id]))
+          $post[$key] = trim(strip_tags(stripslashes($_POST[$key])));
+      }
+    }
+
+    $result = submit_salesforce_form($post, $options);
+
+    if (!$result) {
+      $content = '<strong>' . esc_html(stripslashes($options['sferrormsg'])) . '</strong>';
+    }
+    else {
+      $content = 'Danke';
+    }
+    
+    return $content;
+  }
+
+  function display() {
     $campaignsAsJson = $this->getCampaigns();
     $result_array = array();
     foreach ($campaignsAsJson["records"] as $key => $value) {
-      $tmp = $value["Name"] . $value["Description"];
-        if (isset($_POST['w2lsubmit']) && $_POST['Campaign_ID'] == $value["Id"]) {
-          $tmp = $tmp . "success";
-          $post = array();
-          $post[$id] = trim(strip_tags(stripslashes($_POST[$id])));
-          $options = get_option("salesforce2");
-          submit_salesforce_form($post, $options, 1);
+      $result = array();
+      $result[] = $value["Name"];
+      $result[] = $value["Description"];
+      
+      if (isset($_POST['w2lsubmit']) && $_POST['Campaign_ID'] == $value["Id"]) {
+        $result[]  = $this->submission();
+      }
+      else {
+        $result[]  = $this->createForm($value["Id"]);
+      }
 
-        } else {
-          $tmp = $tmp . $this->createForm($value["Id"]);
-        }
-
-      $result_array[] = $tmp;
+      $result_array[] = implode($result);
     }
     return implode($result_array);
   }
+
 }
 
 //Register Handlers
